65f3a67f153488b8c813c2f00e0251a3531e88a4
pacdt <mateus15do2@gmail.com>
2023-01-27 22:43:59 -0300

    feat: Add New Habit & Day Screen & navigation

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -3,6 +3,7 @@ import { View, Text, ScrollView } from "react-native";
 import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning'
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
+import { useNavigation } from "@react-navigation/native";
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S','S']
 const datesFromYearStart = generateDatesFromYearBeginning();
@@ -10,6 +11,8 @@ const minimumSummaryDatesSizes = 18 * 5;
 const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
 
 export function Home() {
+
+    const { navigate } = useNavigation();
     return (
         <View className="flex-1 bg-background px-8 py-16">
             <Header />
@@ -36,6 +39,7 @@ export function Home() {
                 datesFromYearStart.map(date => (
                     <HabitDay
                     key={date.toISOString()}
+                    onPress={() => navigate('habit', {date: date.toISOString()})}
                     />
                 ))
             }
